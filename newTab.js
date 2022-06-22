0bad8e2b48a6fec1874312a6e077b199717f6572
Dijo Daiju <60286610+dijodaiju7@users.noreply.github.com>
2022-06-23 00:12:36 +0530

    Added download option

--- newTab.js
+++ newTab.js
@@ -10,3 +10,22 @@ document.addEventListener('DOMContentLoaded', function() {
     //to implrment auto-copy
     }, false);
 })
+const fname='convtxt.txt'
+function downloadFile(txtcontent){
+const element=document.createElement('a');
+const blob=new Blob([txtcontent],{type:'plain/text'});
+const fileUrl=URL.createObjectURL(blob);
+element.setAttribute('href',fileUrl);
+element.setAttribute('download',fname);
+element.style.display='none';
+document.body.appendChild(element);
+element.click();
+};
+window.onload=()=>{
+    document.getElementById('download').addEventListener('click',e=>{
+    const txtcontent=document.getElementById('clipboard-paste').innerText;
+    if(txtcontent){
+        downloadFile(txtcontent);
+    }
+    });
+};
